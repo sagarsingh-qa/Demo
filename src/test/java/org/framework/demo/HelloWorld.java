e8afecafa7dc8fc791021503579439af3330df6e
Sagar Singh <bisensagar1234@gmail.com>
2019-12-25 18:03:02 +0530

    adding two new files

---
+++ src/test/java/org/framework/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.framework.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Baby:::");
+	}
+
+}
